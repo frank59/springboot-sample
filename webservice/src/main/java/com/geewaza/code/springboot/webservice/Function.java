eeea8fe76b15aec461aa61f26bc3f8a702ed19cb
wangheng <380330439@qq.com>
2021-03-08 23:04:16 +0800

    增加webservice

--- webservice/src/main/java/com/geewaza/code/springboot/webservice/Function.java
+++ webservice/src/main/java/com/geewaza/code/springboot/webservice/Function.java
@@ -16,6 +16,7 @@ public class Function {
 
     public static void main(String[] args) {
         Endpoint.publish("http://localhost:8089/service/function", new Function());
-        System.out.println("Publish Success:listen port 8089");
+        System.out.println("Publish Success:listen port 8089. visit: http://localhost:8089/service/function?wsdl");
+
     }
 }
